10a3461b2387726fe1cd1176abb78ce498683346
li-exgen <li@exgen.co.jp>
2017-07-07 14:10:12 +0900

    HelloWorld1～5 を外出すように修正

--- webapp/appExample01/src/main/java/local/test/common/App.java
+++ webapp/appExample01/src/main/java/local/test/common/App.java
@@ -7,14 +7,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
-    	 
+
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
         obj.printHello();
-        
+
+        HelloWorld1 obj1 = (HelloWorld1) context.getBean("helloBean1");
+        obj1.printHello1();
+
+        HelloWorld2 obj2 = (HelloWorld2) context.getBean("helloBean2");
+        obj2.printHello2();
+
+        HelloWorld3 obj3 = (HelloWorld3) context.getBean("helloBean3");
+        obj3.printHello3();
+
+        HelloWorld4 obj4 = (HelloWorld4) context.getBean("helloBean4");
+        obj4.printHello4();
+
+        HelloWorld5 obj5 = (HelloWorld5) context.getBean("helloBean5");
+        obj5.printHello5();
+
     }
 }
